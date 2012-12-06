8e29df400954b62d3828a8ee4baa4a4be8e5ae06
Adam Sven Johnson <adam@pkqk.net>
2012-12-06 16:57:32 +0000

    502 means over capacity

--- collect.py
+++ collect.py
@@ -144,7 +144,7 @@ def rate_limited(response):
     Response is rate limited"
     """
 
-    return response.status_code in [420, 429, 500, 503, 504]
+    return response.status_code in [420, 429, 500, 502, 503, 504]
 
 
 if __name__ == "__main__":
