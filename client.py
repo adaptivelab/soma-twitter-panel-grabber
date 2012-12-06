eed86eda91653a8ea64469e19720908961acae41
Adam Sven Johnson <adam@pkqk.net>
2012-12-06 13:17:53 +0000

    store twitter credentials in config.ini

--- client.py
+++ client.py
@@ -4,6 +4,7 @@ Connect to twitter
 
 from __future__ import unicode_literals, print_function
 
+import ConfigParser as configparser
 import time
 import logging
 import random
@@ -16,17 +17,13 @@ from requests.auth import OAuth1
 
 logger = logging.getLogger('wood_panelling')
 
-# twitter oauth details
-CONSUMER_KEY = "client key"
-CONSUMER_SECRET = "client secret"
-REQUEST_TOKEN_URL = "https://api.twitter.com/oauth/request_token"
-AUTHORIZE_URL = "https://api.twitter.com/oauth/authorize"
-ACCESS_TOKEN_URL = "https://api.twitter.com/oauth/access_token"
-ACCESS_TOKEN = "user token"
-ACCESS_TOKEN_SECRET = "user secret"
-
-auth = OAuth1(client_key=CONSUMER_KEY, client_secret=CONSUMER_SECRET,
-    resource_owner_key=ACCESS_TOKEN, resource_owner_secret=ACCESS_TOKEN_SECRET,
+config = configparser.ConfigParser()
+config.read('config.ini')
+twitter_config = partial(config.get, 'twitter')
+auth = OAuth1(client_key=twitter_config('consumer_key'),
+    client_secret=twitter_config('consumer_secret'),
+    resource_owner_key=twitter_config('access_token'),
+    resource_owner_secret=twitter_config('access_token_secret'),
     signature_type='query')
 
 get = partial(requests.get, auth=auth)
