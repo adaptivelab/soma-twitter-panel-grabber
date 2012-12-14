5b2355cf7b59dc5cdedea3a417a7289f6f9abee8
Adam Sven Johnson <adam@pkqk.net>
2012-12-14 15:22:46 +0000

    match up yougov panelist ids with twitter accounts using old panel_default.json file for uk and csv of name, id for usa

--- output.py
+++ output.py
@@ -6,12 +6,43 @@ Update a SoMA panel from collected twitter data
 from __future__ import unicode_literals, print_function
 
 import sys
+import csv
 import json
 
 import data
 
 
-def panelist_info(rs, screen_name, missing_info):
+class YouGovIDMapJSON(dict):
+
+    def __init__(self, filename):
+        with open(filename) as f:
+            data = [json.loads(line) for line in f]
+        for record in data:
+            yougov_id = record['yougov']['id']
+            screen_name = record['twitter']['screen_name']
+            self[screen_name.lower()] = yougov_id
+
+
+class YouGovIDMapCSV(dict):
+
+    def __init__(self, filename):
+        with open(filename) as f:
+            for screen_name, id in csv.reader(f):
+                if screen_name.lower() == 'n/a':
+                    continue
+                screen_name = screen_name.replace('@', '').replace('/', '')
+                self[screen_name.lower()] = id
+
+
+def yougov_idmap(filename):
+    if filename.endswith('.csv'):
+        return YouGovIDMapCSV(filename)
+    elif filename.endswith('.json'):
+        return YouGovIDMapJSON(filename)
+    return {}
+
+
+def panelist_info(rs, screen_name, idmap, missing_info):
     """
     Collect the panelist info from redis
     adding screen_name to a missing set if there is a keyerror
@@ -19,12 +50,14 @@ def panelist_info(rs, screen_name, missing_info):
 
     try:
         info = rs.panelist_info(screen_name)
+        if screen_name.lower() in idmap:
+            info['yougov'] = {'id': idmap[screen_name.lower()]}
         return json.dumps(info)
     except KeyError:
         missing_info.add(screen_name)
 
 
-def export_to_json(filename, missing):
+def export_to_json(filename, idmap, missing):
     """
     Write out everything as json to filename
     missing screen_names to missing
@@ -34,7 +67,7 @@ def export_to_json(filename, missing):
     missing_info = set()
     with open(filename, 'w') as io:
         for screen_name in rs.screen_names:
-            info = panelist_info(rs, screen_name, missing_info)
+            info = panelist_info(rs, screen_name, idmap, missing_info)
             if info:
                 io.write(info)
                 io.write("\n")
@@ -46,9 +79,9 @@ def export_to_json(filename, missing):
 
 if __name__ == "__main__":
     try:
-        filename, missingname = sys.argv[1:3]
+        filename, idmapname, missingname = sys.argv[1:]
     except KeyError:
-        print("Usage: {} filename missingname".format(sys.argv[0]))
+        print("Usage: {} filename yougovid.[json,csv] missingname".format(sys.argv[0]))
         sys.exit(1)
 
-    export_to_json(filename, missingname)
+    export_to_json(filename, yougov_idmap(idmapname), missingname)
